36273c4c26a77c56af404d4760ff4c89fb708eb5
kevinjwesley <kevin.john.wesley@gmail.com>
2021-07-02 11:18:04 -0600

    server connected

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
-<<<<<<< HEAD
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
 
@@ -12,6 +14,19 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch((err) => console.log(err));
 
 server.applyMiddleware({ app });
 
@@ -24,27 +39,3 @@ db.once("open", () => {
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
-=======
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const app = express();
-// Bodyparser middleware
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
-app.use(bodyParser.json());
-// DB Config
-const db = require("./config/keys").mongoURI;
-// Connect to MongoDB
-mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
->>>>>>> master
